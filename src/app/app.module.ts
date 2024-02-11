c096e8e60d5586764e4ae1e45f0e7dd2cadc2dbc
DIR\igor.gralak <igor.gralak>
2024-02-11 11:33:46 +0100

    ngxs/stora and ngs-toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, importProvidersFrom } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgxsModule } from '@ngxs/store';
+import { HttpClientModule } from '@angular/common/http';
+
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,18 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    provideAnimations(),
+    provideToastr({
+      timeOut: 10000,
+      positionClass: 'toast-top-center',
+      preventDuplicates: true,
+    }),
+    importProvidersFrom(
+      NgxsModule.forRoot(),
+      HttpClientModule
+    ),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
