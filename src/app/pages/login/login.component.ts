7e58d4a9b819ecf67b314d9609e3d79aae959dcd
DIR\igor.gralak <igor.gralak>
2024-02-11 15:32:35 +0100

    Changed: Login, Compose, Message, Register Components
    Added Card, MessageItem,ToInput Components

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { CardComponent } from 'src/app/shared/component/card/card.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
